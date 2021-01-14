652a2445a302e9a9dc666c6afed9b67e1c3dd2d8
Petter Iversen <petter.iversen90@gmail.com>
2021-01-14 12:45:08 +0100

    html, css, js

---
+++ script.js
@@ -0,0 +1,23 @@
+const navToggler = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  // toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    // animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 10 + 0.3
+        }s`;
+      }
+    });
+    //   burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navToggler();
