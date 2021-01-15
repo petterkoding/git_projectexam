fb072999710e320c81fb0db066fab0260d99c930
Petter Iversen <petter.iversen90@gmail.com>
2021-01-15 13:59:19 +0100

    fixed flexbox positioning
    added content on index.html page

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 const url = "http://api.open-notify.org/astros.json";
 
-// const pplContainer = document.querySelector(".peopleInSpace")
+const pplContainer = document.querySelector(".peopleInSpace")
 
-// async function getPeopleInSpace() {
-//     try {
-//         const response = await fetch(url);
-//         const json = await response.json();
-//         console.log(json.people)
-//         const people = json.people;
+const peopleInSpace = document.querySelector(".currentPeopleInSpace")
 
-//         pplContainer.innerHTML = "";
+
+
+async function getPeopleInSpace() {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json.people)
+        const people = json.people;
+
+        pplContainer.innerHTML = "";
+        peopleInSpace.innerHTML = "";
         
-//         for (let i = 0; i < people.length; i++){
-//             pplContainer.innerHTML += `<div class="people">
-//                                         <h3>${people[i].name}</h3>
-//                                     </div>`;
-//         }
-//     } catch (error) {
-//         console.log(error)
-//         pplContainer.innerHTML = `an error has occured`;
+        for (let i = 0; i < people.length; i++){
+            pplContainer.classList.add("people");
+            pplContainer.innerHTML += `<div class="people">
+                                        <h4>${people[i].name}</h4>
+                                    </div>`;
+            peopleInSpace.innerHTML = `<span>There are currently ${people.length} people in space</span>`;
+        }
+    } catch (error) {
+        console.log(error)
+        pplContainer.innerHTML = `an error has occured`;
         
-//     }
-// }
+    }
+}
 
-// getPeopleInSpace();
+getPeopleInSpace();
 
 const pastLaunch = "https://api.spacexdata.com/v4/launches/latest";
 
-const latestContainer = document.querySelector(".latest")
+const latestDetails = document.querySelector(".latest-details")
 
-latestContainer.innerHTML = "";
+latestDetails.innerHTML = "";
+
 
 async function getLatestLaunch(){
     try{
         const response = await fetch(pastLaunch);
         const result = await response.json();
         console.log(result)
-        latestContainer.innerHTML = `<div>
-                                    <h2>Latest launch</h2>
-                                    <h3 class="rocketName">${result.name}</h3>
-                                    <span class="date">${result.date_local}</span>
-                                    <div class="para">
-                                        <p class="details">${result.details}</p>
-                                    </div>                         
+        latestDetails.classList.add("turksat")
+        latestDetails.innerHTML = `<div class="turksat">
+                                        <h3>Latest</h3>
+                                        <h2 class="rocketName">${result.name}</h2>
+                                        <span class="launch-date">${result.date_local}</span>
+                                        <div class="para">
+                                            <p class="details">${result.details}</p>
+                                        </div>
+                                        <a class="button dark" href="https://spacenews.com/spacex-launches-turksat-5a/" target="_blank">Read more</a>                    
+                                        <a class="button dark" href="https://youtu.be/9I0UYXVqIn8" target="_blank">Watch stream</a>                    
                                     </div>`;
 
     } catch (e) {
